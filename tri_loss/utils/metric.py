beb7ce24db2ea65ae3d12f779b7bb88c0f6039d8
Anurag Singh <tellanurag95@gmail.com>
2019-11-08 14:52:32 +0530

    Update metric.py

--- tri_loss/utils/metric.py
+++ tri_loss/utils/metric.py
@@ -19,55 +19,6 @@ def _unique_sample(ids_dict, num):
     mask[i] = True
   return mask
 
-def extend_to_topk(ret,topk):
-    tail=ret[-1]*np.ones(topk-ret.shape[0])
-    return np.concatenate((ret,tail),axis=None)
-
-def cmc_prid(
-    distmat,
-    query_ids=None,
-    gallery_ids=None,
-    query_cams=None,
-    gallery_cams=None,
-    topk=10,
-    separate_camera_set=False,
-    single_gallery_shot=False,
-    first_match_break=False,
-    average=True):
-  """
-  Args:
-    distmat: numpy array with shape [num_query, num_gallery], the 
-      pairwise distance between query and gallery samples
-    query_ids: numpy array with shape [num_query]
-    gallery_ids: numpy array with shape [num_gallery]
-    query_cams: numpy array with shape [num_query]
-    gallery_cams: numpy array with shape [num_gallery]
-    average: whether to average the results across queries
-  Returns:
-    If `average` is `False`:
-      ret: numpy array with shape [num_query, topk]
-      is_valid_query: numpy array with shape [num_query], containing 0's and 
-        1's, whether each query is valid or not
-    If `average` is `True`:
-      numpy array with shape [topk]
-  """
-  # Ensure numpy array
-  assert isinstance(distmat, np.ndarray)
-  assert isinstance(query_ids, np.ndarray)
-  assert isinstance(gallery_ids, np.ndarray)
-  assert isinstance(query_cams, np.ndarray)
-  assert isinstance(gallery_cams, np.ndarray)
-
-  m, n = distmat.shape
-  # Sort and find correct matches
-  indices = np.argsort(distmat, axis=1)
-  matches = (gallery_ids[indices] == query_ids[:, np.newaxis])
-  print("matches.shape is: ",matches.shape," and the rank 1 is: ",np.sum(matches[:,0]==True)/m)
-  matches=matches[:,:topk]
-  for j in range(1,topk):
-      matches[:,j]=matches[:,j]|matches[:,j-1]
-  ret=np.sum(matches, axis=0)/m
-  return ret
 
 def cmc(
     distmat,
@@ -75,7 +26,7 @@ def cmc(
     gallery_ids=None,
     query_cams=None,
     gallery_cams=None,
-    topk=10,
+    topk=100,
     separate_camera_set=False,
     single_gallery_shot=False,
     first_match_break=False,
@@ -152,53 +103,6 @@ def cmc(
     return np.sum(ret, axis=0) / num_valid_queries
   return ret, is_valid_query
 
-
-def mean_ap_prid(
-    distmat,
-    query_ids=None,
-    gallery_ids=None,
-    query_cams=None,
-    gallery_cams=None,
-    average=True):
-  """
-  Args:
-    distmat: numpy array with shape [num_query, num_gallery], the 
-      pairwise distance between query and gallery samples
-    query_ids: numpy array with shape [num_query]
-    gallery_ids: numpy array with shape [num_gallery]
-    query_cams: numpy array with shape [num_query]
-    gallery_cams: numpy array with shape [num_gallery]
-    average: whether to average the results across queries
-  Returns:
-    If `average` is `False`:
-      ret: numpy array with shape [num_query]
-      is_valid_query: numpy array with shape [num_query], containing 0's and 
-        1's, whether each query is valid or not
-    If `average` is `True`:
-      a scalar
-  """
-
-  # Ensure numpy array
-  assert isinstance(distmat, np.ndarray)
-  assert isinstance(query_ids, np.ndarray)
-  assert isinstance(gallery_ids, np.ndarray)
-  assert isinstance(query_cams, np.ndarray)
-  assert isinstance(gallery_cams, np.ndarray)
-
-  m, n = distmat.shape
-
-  # Sort and find correct matches
-  indices = np.argsort(distmat, axis=1)
-  matches = (gallery_ids[indices] == query_ids[:, np.newaxis])
-  # Compute AP for each query
-  aps = np.zeros(m)
-  for i in range(m):
-      count_positive_samples=0
-      for j in range(n):
-          if matches[i][j]==True:
-               count_positive_samples+=1
-               aps[i]+=(count_positive_samples/(j+1))
-  return float(np.sum(aps)) / m
 
 def mean_ap(
     distmat,
