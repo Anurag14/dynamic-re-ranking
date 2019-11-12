4fabaff46a804d98cd278a437fb9925a8d1816d5
Anurag Singh <tellanurag95@gmail.com>
2019-11-13 01:40:47 +0530

    Update new_re_ranking.py

--- tri_loss/utils/new_re_ranking.py
+++ tri_loss/utils/new_re_ranking.py
@@ -38,26 +38,34 @@ def re_ranking(features, q_g_dist, q_q_dist, g_g_dist, k1=20, k2=6, lambda_value
     # Don't care about it.
     #append all in A_big 
     big_A=[]
-    alpha_crop_gallery=51
+    alpha_crop_gallery=50
     parameters={'alpha': 0.1,'beta':0.1,'gamma':10,'lambda':1,'epsilon':k1,'eta':0,'iterations':50}
     for probe_index in range(len(q_g_dist)):
-      index_of_features=[]
-      index_of_features=np.argpartition(q_g_dist[probe_index],alpha_crop_gallery_probe)
-      with torch.no_grad():
-        X=torch.transpose(features[index_of_features[:all_crop_gallery_probe]],0,1)
-        parameters['eta']=torch.sum(X**2)
-        A=convex_update(X,parameters)
-        big_A.append(A.detach().numpy())
+        index_of_features=[]
+        index_of_features=np.argpartition(q_g_dist[probe_index],alpha_crop_gallery_probe+1)
+        with torch.no_grad():
+            X=torch.transpose(features[index_of_features[:all_crop_gallery_probe+1]],0,1)
+            parameters['eta']=torch.sum(X**2)
+            A=convex_update(X,parameters)
+            big_A.append(A.detach().numpy())
     for A in big_A:
-      V = np.zeros_like(A).astype(np.float32)
-      initial_rank = np.argsort(A[:][0]).astype(np.int32)
-      forward_k_neigh_index = initial_rank[:epsilon]
-      backward_k_neigh_index = np.where(A[0][forward_k_neigh_index]>0)
-
-    query_num = q_g_dist.shape[0]
-    gallery_num = q_g_dist.shape[0] + q_g_dist.shape[1]
-    all_num = gallery_num
-
+        V = np.zeros_like(A).astype(np.float32)
+        initial_rank = np.argsort(A[:][0]).astype(np.int32)
+        forward_k_neigh_index = initial_rank[:k1+1]
+        backward_k_neigh_index = np.where(A[:][forward_k_neigh_index]>0)
+        fi = np.where(backward_k_neigh_index==i)[0]
+        k_reciprocal_index = forward_k_neigh_index[fi]
+        k_reciprocal_expansion_index = k_reciprocal_index
+        for j in range(len(k_reciprocal_index)):
+            candidate = k_reciprocal_index[j]
+            candidate_forward_k_neigh_index = initial_rank[candidate,:int(np.around(k1/2.))+1]
+            candidate_backward_k_neigh_index = initial_rank[candidate_forward_k_neigh_index,:int(np.around(k1/2.))+1]
+            fi_candidate = np.where(candidate_backward_k_neigh_index == candidate)[0]
+            candidate_k_reciprocal_index = candidate_forward_k_neigh_index[fi_candidate]
+            if len(np.intersect1d(candidate_k_reciprocal_index,k_reciprocal_index))> 2./3*len(candidate_k_reciprocal_index):
+                k_reciprocal_expansion_index = np.append(k_reciprocal_expansion_index,candidate_k_reciprocal_index)
+                
+                
     for i in range(all_num):
         # k-reciprocal neighbors
         forward_k_neigh_index = initial_rank[i,:k1+1]
